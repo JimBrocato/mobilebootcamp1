3bf694289c7c584ae95eb0f62a35b779dc6c7daf
JimBrocato <jimbrocato@hotmail.com>
2021-01-06 08:59:10 -0500

    module 3 complete

--- app/src/main/java/com/wcg/mobilebootcamp1_jim/MainActivity.kt
+++ app/src/main/java/com/wcg/mobilebootcamp1_jim/MainActivity.kt
@@ -53,7 +53,7 @@ class MainActivity : AppCompatActivity() {
         navController = navHostFrag.navController
 
         val bottomNavigationView = findViewById<BottomNavigationView>(R.id.bottom_nav)
-        val navGraphIds = listOf(R.navigation.nav_graph_main)
+        //val navGraphIds = listOf(R.navigation.nav_graph_main)
         bottomNavigationView.setupWithNavController(navController)
 
         val drawerLayout: DrawerLayout? = findViewById(R.id.drawer_layout)
@@ -64,26 +64,26 @@ class MainActivity : AppCompatActivity() {
         setupActionBarWithNavController(navController, appBarConfiguration)
     }
 
-    private fun setupDrawerNav() {
-        //val appBarConfiguration = AppBarConfiguration(setOf(R.id.nav_graph_home, R.id.nav_graph_search))
-
-        //val navHostFrag : NavHostFragment = findViewById(R.id.nav_host_fragment)
-        val navHostFrag = supportFragmentManager.findFragmentById(R.id.nav_host_fragment) as NavHostFragment
-        val navController = navHostFrag.navController
-
-        //val toolbar = findViewById<Toolbar>(R.id.toolbar)
-
-        //val appBarConfiguration = AppBarConfiguration(navController.graph, findViewById(R.id.drawer_layout))
-        //val appBarConfiguration = AppBarConfiguration(setOf(R.id.nav_graph_home, R.id.nav_graph_search))
-        //setupActionBarWithNavController(navController, appBarConfiguration)
-        val drawerLayout: DrawerLayout? = findViewById(R.id.drawer_layout)
-        //val appBarConfiguration = AppBarConfiguration(setOf(R.id.nav_graph_home, R.id.nav_graph_search), drawerLayout)
-        val appBarConfiguration = AppBarConfiguration(navController.graph, drawerLayout)
-        setupActionBarWithNavController(navController, appBarConfiguration)
-
-        findViewById<NavigationView>(R.id.nav_view).setupWithNavController(navController)
-
-    }
+//    private fun setupDrawerNav() {
+//        //val appBarConfiguration = AppBarConfiguration(setOf(R.id.nav_graph_home, R.id.nav_graph_search))
+//
+//        //val navHostFrag : NavHostFragment = findViewById(R.id.nav_host_fragment)
+//        val navHostFrag = supportFragmentManager.findFragmentById(R.id.nav_host_fragment) as NavHostFragment
+//        val navController = navHostFrag.navController
+//
+//        //val toolbar = findViewById<Toolbar>(R.id.toolbar)
+//
+//        //val appBarConfiguration = AppBarConfiguration(navController.graph, findViewById(R.id.drawer_layout))
+//        //val appBarConfiguration = AppBarConfiguration(setOf(R.id.nav_graph_home, R.id.nav_graph_search))
+//        //setupActionBarWithNavController(navController, appBarConfiguration)
+//        val drawerLayout: DrawerLayout? = findViewById(R.id.drawer_layout)
+//        //val appBarConfiguration = AppBarConfiguration(setOf(R.id.nav_graph_home, R.id.nav_graph_search), drawerLayout)
+//        val appBarConfiguration = AppBarConfiguration(navController.graph, drawerLayout)
+//        setupActionBarWithNavController(navController, appBarConfiguration)
+//
+//        findViewById<NavigationView>(R.id.nav_view).setupWithNavController(navController)
+//
+//    }
 
     override fun onOptionsItemSelected(item: MenuItem): Boolean {
         return item.onNavDestinationSelected(navController)
@@ -93,30 +93,30 @@ class MainActivity : AppCompatActivity() {
     }
 
 
-    private fun setupBottomNavigationBar() {
-        //https://github.com/android/architecture-components-samples/blob/main/NavigationAdvancedSample/app/src/main/java/com/example/android/navigationadvancedsample/MainActivity.kt
-
-        //val bottomNavigationView = binding.bottomNav
-        //val bottomNavigationView = findNavController(R.id.bottom_nav)
-        val bottomNavigationView = findViewById<BottomNavigationView>(R.id.bottom_nav)
-
-        //val navGraphIds = listOf(R.navigation.nav_graph_home, R.navigation.nav_graph_shop, R.navigation.nav_graph_search, R.navigation.nav_graph_friends)
-        val navGraphIds = listOf(R.navigation.nav_graph_main)
-
-        // Setup the bottom navigation view with a list of navigation graphs
-        val controller = bottomNavigationView.setupWithNavController(
-            navGraphIds = navGraphIds,
-            fragmentManager = supportFragmentManager,
-            containerId = R.id.nav_host_fragment,
-            intent = intent
-        )
-
-        // Whenever the selected controller changes, setup the action bar.
-//        controller.observe(this, Observer { navController ->
-//            setupActionBarWithNavController(navController)
-//        })
-        currentNavController = controller
-    }
+//    private fun setupBottomNavigationBar() {
+//        //https://github.com/android/architecture-components-samples/blob/main/NavigationAdvancedSample/app/src/main/java/com/example/android/navigationadvancedsample/MainActivity.kt
+//
+//        //val bottomNavigationView = binding.bottomNav
+//        //val bottomNavigationView = findNavController(R.id.bottom_nav)
+//        val bottomNavigationView = findViewById<BottomNavigationView>(R.id.bottom_nav)
+//
+//        //val navGraphIds = listOf(R.navigation.nav_graph_home, R.navigation.nav_graph_shop, R.navigation.nav_graph_search, R.navigation.nav_graph_friends)
+//        val navGraphIds = listOf(R.navigation.nav_graph_main)
+//
+//        // Setup the bottom navigation view with a list of navigation graphs
+//        val controller = bottomNavigationView.setupWithNavController(
+//            navGraphIds = navGraphIds,
+//            fragmentManager = supportFragmentManager,
+//            containerId = R.id.nav_host_fragment,
+//            intent = intent
+//        )
+//
+//        // Whenever the selected controller changes, setup the action bar.
+////        controller.observe(this, Observer { navController ->
+////            setupActionBarWithNavController(navController)
+////        })
+//        currentNavController = controller
+//    }
 
     override fun onSupportNavigateUp(): Boolean {
         //return currentNavController?.value?.navigateUp() ?: false  // use this w/ bottom nav only
